6cb400dabe4ec22821b11612fc61310c2ee1e99a
Horacio Gomez-Acevedo <gomezacevedohoracio@uams.edu>
2026-09-21 10:02:09 -0500

    adding a python file

---
+++ labs/Session_7/basic_input.py
@@ -0,0 +1,9 @@
+import sys
+
+argument1=sys.argv[1]
+argument2=sys.argv[2]
+veryfirst=sys.argv[0]
+
+print(f"The first argument was {argument1}")
+print(f"and the second argument was {argument2}")
+print(f"and the very first argument was {veryfirst}")
